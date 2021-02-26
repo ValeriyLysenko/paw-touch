57afe69184e9f49a8278da94793d1ad444a7474c
Lysenko Valeriy <ahhoo2000@gmail.com>
2021-02-26 15:04:40 +0300

    Airbnb eslint config is added.

---
+++ shared/airbnb-rules-correction.js
@@ -0,0 +1,384 @@
+module.exports = {
+    rules: {
+        // disallow use of console
+        'no-console': 'off',
+
+        // this option sets a specific tab width for your code
+        // https://eslint.org/docs/rules/indent
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+                // MemberExpression: null,
+                FunctionDeclaration: {
+                    parameters: 1,
+                    body: 1,
+                },
+                FunctionExpression: {
+                    parameters: 1,
+                    body: 1,
+                },
+                CallExpression: {
+                    arguments: 1,
+                },
+                ArrayExpression: 1,
+                ObjectExpression: 1,
+                ImportDeclaration: 1,
+                flatTernaryExpressions: false,
+                // list derived from https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
+                ignoredNodes: [
+                    'JSXElement',
+                    'JSXElement > *',
+                    'JSXAttribute',
+                    'JSXIdentifier',
+                    'JSXNamespacedName',
+                    'JSXMemberExpression',
+                    'JSXSpreadAttribute',
+                    'JSXExpressionContainer',
+                    'JSXOpeningElement',
+                    'JSXClosingElement',
+                    'JSXText',
+                    'JSXEmptyExpression',
+                    'JSXSpreadChild',
+                ],
+                ignoreComments: false,
+            },
+        ],
+
+        // disallow dangling underscores in identifiers
+        'no-underscore-dangle': 'off',
+
+        // disallow padding within blocks
+        'padded-blocks': 'off',
+
+        // allow just one var statement per function
+        'one-var': 'off',
+
+        // use rest parameters instead of arguments
+        // http://eslint.org/docs/rules/prefer-rest-params
+        'prefer-rest-params': 'off',
+
+        // disallow use of unary operators, ++ and --
+        // http://eslint.org/docs/rules/no-plusplus
+        'no-plusplus': 'off',
+
+        // require padding inside curly braces
+        'object-curly-spacing': ['error', 'never'],
+
+        // disallow mixes of different operators
+        // http://eslint.org/docs/rules/no-mixed-operators
+        'no-mixed-operators': [
+            'error',
+            {
+                groups: [
+                    ['+', '-', '*', '/', '%', '**'],
+                    ['&', '|', '^', '~', '<<', '>>', '>>>'],
+                    ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+                    ['&&', '||'],
+                    ['in', 'instanceof'],
+                ],
+                allowSamePrecedence: true,
+            },
+        ],
+
+        // disallow reassignment of function parameters
+        // disallow parameter object manipulation except for specific exclusions
+        // rule: http://eslint.org/docs/rules/no-param-reassign.html
+        'no-param-reassign': [
+            'error',
+            {
+                props: true,
+                ignorePropertyModificationsFor: [
+                    '$', // jQuery object
+                ],
+            },
+        ],
+
+        // specify the maximum length of a line in your program
+        // http://eslint.org/docs/rules/max-len
+        'max-len': [
+            'warn',
+            150,
+            4,
+            {
+                ignoreUrls: true,
+                ignoreComments: false,
+                ignoreRegExpLiterals: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+
+        // disallow declaration of variables that are not used in the code
+        'no-unused-vars': [
+            'warn',
+            {vars: 'all', args: 'none', ignoreRestSiblings: true},
+        ],
+
+        // !!!TEMP
+        // disallow lexical declarations in case/default clauses
+        // http://eslint.org/docs/rules/no-case-declarations.html
+        'no-case-declarations': 'warn',
+
+        // enforce that class methods use "this"
+        // http://eslint.org/docs/rules/class-methods-use-this
+        'class-methods-use-this': 'off',
+
+        // disallow use of variables before they are defined
+        'no-use-before-define': [
+            'error',
+            {functions: false, classes: true, variables: true},
+        ],
+
+        // disallow use of assignment in return statement
+        'no-return-assign': ['warn', 'always'],
+
+        // enforces return statements in callbacks of array"s methods
+        // http://eslint.org/docs/rules/array-callback-return
+        'array-callback-return': 'warn',
+
+        // !!!TEMP
+        // require return statements to either always or never specify values
+        'consistent-return': 'off',
+
+        // Prefer destructuring from arrays and objects
+        // http://eslint.org/docs/rules/prefer-destructuring
+        'prefer-destructuring': [
+            'warn',
+            {
+                VariableDeclarator: {
+                    array: false,
+                    object: true,
+                },
+                AssignmentExpression: {
+                    array: true,
+                    object: true,
+                },
+            },
+            {
+                enforceForRenamedProperties: false,
+            },
+        ],
+
+        // !!!TEMP
+        // disallow specific globals
+        // "no-restricted-globals": "off",
+
+        // require all requires be top-level
+        // http://eslint.org/docs/rules/global-require
+        'global-require': 'off',
+
+        // disallow if as the only statement in an else block
+        // http://eslint.org/docs/rules/no-lonely-if
+        'no-lonely-if': 'off',
+
+        // disallow use of the continue statement
+        // http://eslint.org/docs/rules/no-continue
+        'no-continue': 'off',
+
+        // require function expressions to have a name
+        // https://eslint.org/docs/rules/func-names
+        'func-names': 'off',
+
+        // disallow use of labels for anything other then loops and switches
+        'no-labels': 'off',
+
+        // disallow nested ternary expressions
+        'no-nested-ternary': 'off',
+
+        // disallow certain syntax forms
+        // https://eslint.org/docs/rules/no-restricted-syntax
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForInStatement',
+                message:
+                    'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'WithStatement',
+                message:
+                    '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
+
+        // disallow redundant `return await`
+        'no-return-await': 'off',
+
+        // require a capital letter for constructors
+        'new-cap': [
+            'warn',
+            {
+                newIsCap: true,
+                newIsCapExceptions: [],
+                capIsNew: false,
+                capIsNewExceptions: [
+                    'Immutable.Map',
+                    'Immutable.Set',
+                    'Immutable.List',
+                ],
+            },
+        ],
+
+        // disallow use of bitwise operators
+        // https://eslint.org/docs/rules/no-bitwise
+        'no-bitwise': 'off',
+
+        /**
+         * Import
+         *
+         */
+
+        // disallow non-import statements appearing before import statements
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
+        'import/first': 'off',
+
+        // Ensure consistent use of file extension within the import path
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+        'import/extensions': 'off',
+
+        // !!!TEMP
+        // ensure imports point to files/modules that can be resolved
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
+        'import/no-unresolved': 'off',
+
+        // Forbid require() calls with expressions
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
+        'import/no-dynamic-require': 'warn',
+
+        /**
+         * React
+         *
+         */
+
+        // Prevent React to be incorrectly marked as unused
+        'react/jsx-uses-react': 'off',
+        // Prevent missing React when using JSX
+        'react/react-in-jsx-scope': 'off',
+
+        // Forbid certain propTypes (any, array, object)
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
+        'react/forbid-prop-types': 'off',
+
+        // Enforce a defaultProps definition for every prop that is not a required prop
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
+        'react/require-default-props': 'off',
+
+        // only .jsx files may have JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+        'react/jsx-filename-extension': [
+            'error',
+            {extensions: ['.jsx', '.tsx', '.js', '.ts']},
+        ],
+
+        // Enforce JSX indentation
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
+        'react/jsx-indent': ['error', 4],
+
+        // Validate props indentation in JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
+        'react/jsx-indent-props': ['error', 4],
+
+        // Enforce component methods order
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
+        'react/sort-comp': 'off',
+
+        // ensure <a> tags are valid
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/0745af376cdc8686d85a361ce36952b1fb1ccf6e/docs/rules/anchor-is-valid.md
+        'jsx-a11y/anchor-is-valid': 'off',
+
+        // Require stateless functions when not using lifecycle methods, setState or ref
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+        'react/prefer-stateless-function': [
+            'warn',
+            {ignorePureComponents: true},
+        ],
+
+        // !!!TEMP
+        // Prevent usage of Array index in keys
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
+        'react/no-array-index-key': 'warn',
+
+        // require onClick be accompanied by onKeyUp/onKeyDown/onKeyPress
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/click-events-have-key-events.md
+        'jsx-a11y/click-events-have-key-events': 'off',
+
+        // Prevent multiple component definition per file
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
+        'react/no-multi-comp': ['warn', {ignoreStateless: true}],
+
+        // Prevent unused propType definitions
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
+        'react/no-unused-prop-types': [
+            'warn',
+            {
+                customValidators: [],
+                skipShapeProps: true,
+            },
+        ],
+
+        // Prevent usage of button elements without an explicit type attribute
+        // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/button-has-type.md
+        'react/button-has-type': 'off',
+
+        // Enforce consistent usage of destructuring assignment of props, state, and context
+        // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/destructuring-assignment.md
+        'react/destructuring-assignment': ['warn', 'always'],
+
+        // require that JSX labels use "htmlFor"
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+        'jsx-a11y/label-has-for': 'off',
+
+        // Enforce that elements with ARIA roles must have all required attributes
+        // for that role.
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/role-has-required-aria-props.md
+        'jsx-a11y/role-has-required-aria-props': 'warn',
+
+        // Prevent duplicate props in JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
+        'react/jsx-no-duplicate-props': ['error', {ignoreCase: false}],
+
+        // Prevent missing props validation in a React component definition
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+        'react/prop-types': [
+            'off',
+            {
+                ignore: ['children'],
+                customValidators: [],
+                skipUndeclared: false,
+            },
+        ],
+
+        // Enforce that a label tag has a text label and an associated control.
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/b800f40a2a69ad48015ae9226fbe879f946757ed/docs/rules/label-has-associated-control.md
+        'jsx-a11y/label-has-associated-control': [
+            'warn',
+            {
+                labelComponents: [],
+                labelAttributes: [],
+                controlComponents: [],
+                assert: 'both',
+                depth: 25,
+            },
+        ],
+
+        // Enforce boolean attributes notation in JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+        'react/jsx-boolean-value': 'off',
+
+        // Prevent usage of dangerous JSX properties
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
+        'react/no-danger': 'off',
+
+        // Prevent invalid characters from appearing in markup
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
+        'react/no-unescaped-entities': 'warn',
+
+        'react/jsx-props-no-spreading': 'off',
+
+        'react/state-in-constructor': 'off',
+    },
+};
