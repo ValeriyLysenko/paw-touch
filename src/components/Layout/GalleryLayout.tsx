706599d619fe9df0c7c984fe16f5c1f7d991d3d6
Lysenko Valeriy <ahhoo2000@gmail.com>
2021-06-22 00:59:12 +0300

    Advanced design for gallery was added.

--- src/components/Layout/GalleryLayout.tsx
+++ src/components/Layout/GalleryLayout.tsx
@@ -50,7 +50,16 @@ const GalleryLayout: FC<Props> = observer(() => {
     console.log('%cGalleryLayout ===>', 'color: tomato', gallery);
 
     if (!gallery.length) {
-        return <div className="pt-gallery" />;
+        return (
+            <div className="pt-on-top pt-on-top-canvas">
+                <div className="pt-navbar pt-navbar-top">
+                    <GalleryControls />
+                </div>
+                <div className="pt-canvas-container">
+                    <div className="pt-gallery-wrapper" />
+                </div>
+            </div>
+        );
     }
 
     return (
